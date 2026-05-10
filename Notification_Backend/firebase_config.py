6d404830818f0b8736ceeff8bba356f99676c07b
HassanZekrallah <hassan2206066@miuegypt.edu.eg>
2026-05-11 00:46:48 +0300

    trying deploy

--- Notification_Backend/firebase_config.py
+++ Notification_Backend/firebase_config.py
@@ -1,12 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_creds = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
 
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(firebase_creds)
+
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-print("🔥 CONNECTED FIREBASE PROJECT:")
-print(cred.project_id)
