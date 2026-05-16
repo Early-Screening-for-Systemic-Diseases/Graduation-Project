5329cd6251afdf670b51d0cdbc714b7e977676d5
HassanZekrallah <hassan2206066@miuegypt.edu.eg>
2026-05-16 19:23:34 +0300

    ...

--- Notification_Backend/firebase_config.py
+++ Notification_Backend/firebase_config.py
@@ -10,3 +10,13 @@ cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+#for local testing
+# import firebase_admin
+# from firebase_admin import credentials, firestore, messaging
+
+# cred = credentials.Certificate("serviceAccountKey.json")
+
+# firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
